bc1783a0687f5e10af15411d27852dcb335a0dc6
viniciusxv27 <vinicius8cm@gmail.com>
2025-05-29 11:35:23 -0300

    commit inicial

---
+++ app.py
@@ -0,0 +1,93 @@
+from flask import Flask, render_template, request, redirect, url_for
+import pandas as pd
+import os
+import requests
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+logado = False
+load_dotenv()
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        # Aqui você pode adicionar lógica de autenticação
+        
+        if username == str(os.getenv("USERNAME")) and password == str(os.getenv("PASSWORD")):
+            global logado
+            logado = True
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html', error='Credenciais inválidas')
+    return render_template('login.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():# Verifica se o usuário está autenticado
+    if not logado:
+        return redirect(url_for('login'))
+
+    if request.method == 'POST':
+        file = request.files['excel_file']
+        message = request.form['message']
+        leads = []
+
+        if test_number:
+            try:
+                if file and file.filename.endswith('.xlsx'):
+                    filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+                    file.save(filepath)
+
+                    df = pd.read_excel(filepath)
+                    data_list = df.to_dict(orient='records')  # Converte cada linha em um dicionário
+
+                    # Aqui você pode usar os dados e aplicar lógica de envio, por exemplo:
+                    print("Mensagem escrita:", message)
+                    print("Dados importados:")
+                    for row in data_list:
+                        filial = row.get('Filial')
+                        data = row.get('Data')
+                        nome = row.get('Nome Cliente')
+                        plano = row.get('Plano')
+                        telefone = row.get('Acesso')
+
+                        leads.append({
+                            'filial': str(filial),
+                            'data': str(data),
+                            'nome': str(nome),
+                            'plano': str(plano),
+                            'telefone': str(telefone)
+                        })
+
+                    payload = {
+                        'message': message,
+                        'leads': leads
+                    }
+
+                    requests.post('https://rede-confianca-n8n.lpl0df.easypanel.host/webhook-test/disparo-rede-confianca', json=payload)  # Substitua pela URL do seu endpoint
+                    return render_template('index.html', success=True, data=payload)
+                
+            except ValueError:
+                if file and file.filename.endswith('.xlsx'):
+                    filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+                    file.save(filepath)
+
+                    df = pd.read_excel(filepath)
+                    data_list = df.to_dict(orient='records')  # Converte cada linha em um dicionário
+
+                    # Aqui você pode usar os dados e aplicar lógica de envio, por exemplo:
+                    print("Mensagem escrita:", message)
+                    print("Dados importados:")
+                    for row in data_list:
+                        print(row)
+
+                    return render_template('index.html', success=True, data=data_list)
+
+    return render_template('index.html', success=False)
+
+if __name__ == '__main__':
+    app.run(debug=True)
