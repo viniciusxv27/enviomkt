12d3013f077e2d63e3728979cc564c2c38b071b4
Vinicius <83793571+viniciusxv27@users.noreply.github.com>
2025-07-24 13:31:53 -0300

    fix: ajuste app

--- app.py
+++ app.py
@@ -109,4 +109,4 @@ def index():# Verifica se o usuário está autenticado
     return render_template('index.html', success=False)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
